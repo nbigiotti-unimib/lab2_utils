d37d358a6017cfe7b1138daee732f3c788fe8629
Nicolas Bigiotti <nicobigio01@dell-laptop>
2022-02-21 15:08:48 +0100

    minor changes

--- scripts/root_install.sh
+++ scripts/root_install.sh
@@ -9,6 +9,6 @@ else
     wget https://root.cern/download/root_v6.24.02.Linux-ubuntu20-x86_64-gcc9.3.tar.gz
     tar -xzf root_v6.24.02.Linux-ubuntu20-x86_64-gcc9.3.tar.gz
     rm root_v*
-    #apt-get install git dpkg-dev cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev tar gfortran subversion
+    apt-get install git dpkg-dev cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev tar gfortran subversion
 fi
 #pip install root_numpy # optional if you want to convert Trees to numpy arrays and other things like that
