bed92e654fb1d51a769b6537ac34ef0fff3e4303
Nicolas Bigiotti <nicobigio01@dell-laptop>
2022-02-21 14:52:14 +0100

    Removed verbose option while uncompressing

--- scripts/root_install.sh
+++ scripts/root_install.sh
@@ -8,10 +8,11 @@ then
 else
     echo "Downloading and installing pyROOT"
     wget https://root.cern/download/root_v6.24.02.Linux-ubuntu20-x86_64-gcc9.3.tar.gz
-    tar -xzvf root_v6.24.02.Linux-ubuntu20-x86_64-gcc9.3.tar.gz
+    tar -xzf root_v6.24.02.Linux-ubuntu20-x86_64-gcc9.3.tar.gz
     mkdir $ROOT_DIR
     mv root/* $ROOT_DIR
     rm -d root
+    rm root_v*
     #apt-get install git dpkg-dev cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev tar gfortran subversion
 fi
 #pip install root_numpy # optional if you want to convert Trees to numpy arrays and other things like that
