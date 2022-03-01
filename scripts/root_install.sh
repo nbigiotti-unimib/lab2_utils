83237b521ab9e69c8f184f993718acc80177aa45
Nicolas Bigiotti <nicobigio01@dell-laptop>
2022-03-01 10:50:37 +0100

    trying with a different version of root

--- scripts/root_install.sh
+++ scripts/root_install.sh
@@ -11,7 +11,9 @@ else
     wget https://github.com/MohamedElashri/HEP-ML/releases/download/ROOT/ROOT.tar.zip
     unzip /content/ROOT.tar.zip
     tar -xf  ROOT.tar
-    rm /content/ROOT.tar
+    rm /content/ROOT.tar /content/ROOT.tar.zip
+    mkdir /content/root
+    mv -dr content/root_build/* root
     apt-get install git dpkg-dev cmake g++ gcc binutils libx11-dev libxpm-dev libxft-dev libxext-dev tar gfortran subversion
 fi
 #pip install root_numpy # optional if you want to convert Trees to numpy arrays and other things like that
